137763172036fab51246e59ada0f22463e37ec7d
Ahmed Amin <69634124+void0x11@users.noreply.github.com>
2023-11-10 22:28:32 -0700

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-# Usage: ./create_user.sh username
+# Usage: ./create_user.sh username groupname
 # Make sure to run this script as root or with sudo privileges
 
 # Variables
 USERNAME=$1
+GROUPNAME=$2
 MAX_DAYS=90
 MIN_DAYS=10
 WARN_DAYS=7
 
-# Check if a username is provided
-if [ -z "$USERNAME" ]; then
-    echo "Usage: $0 username"
+# Check if a username and groupname are provided
+if [ -z "$USERNAME" ] || [ -z "$GROUPNAME" ]; then
+    echo "Usage: $0 username groupname"
     exit 1
 fi
 
-# Create a new user and ask to set a password
-adduser $USERNAME
+# Check if the provided group exists
+if ! getent group "$GROUPNAME" > /dev/null; then
+    echo "The group '$GROUPNAME' does not exist."
+    exit 1
+fi
+
+# Create a new user and assign the existing group
+adduser --ingroup "$GROUPNAME" "$USERNAME"
 
 # Set password policies
-chage -M $MAX_DAYS -m $MIN_DAYS -W $WARN_DAYS $USERNAME
+chage -M "$MAX_DAYS" -m "$MIN_DAYS" -W "$WARN_DAYS" "$USERNAME"
 
 # Force password change on first login
-chage -d 0 $USERNAME
+chage -d 0 "$USERNAME"
 
-echo "$USERNAME has been created with enforced password policies."
+echo "$USERNAME has been created with enforced password policies and added to the $GROUPNAME group."
